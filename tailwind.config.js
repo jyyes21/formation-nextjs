eb82e70129756a3b3952231f045a70c9597129fe
Jonas Garnero <jonas.garnero@opkod.io>
2022-09-06 12:35:20 +0200

    banniere-fini

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html, js, ts, vue}", "./src/**/*"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    colors: {
-      blue: "#1fb6ff",
-      purple: "#7e5bef",
-      pink: "#ff49db",
-      orange: "#ff7849",
-      green: "#13ce66",
-      yellow: "#ffc82c",
-      "gray-dark": "#273444",
-      gray: "#8492a6",
-      "gray-light": "#d3dce6",
-    },
-    fontFamily: {
-      sans: ["Graphik", "sans-serif"],
-      serif: ["Merriweather", "serif"],
-    },
-    extend: {
-      spacing: {
-        "8xl": "96rem",
-        "9xl": "128rem",
-      },
-      borderRadius: {
-        "4xl": "2rem",
-      },
-
-      plugins: {
-        tailwindcss: { config: "./tailwindcss-config.js" },
-      },
-    },
+    extend: {},
   },
+  plugins: [],
 };
